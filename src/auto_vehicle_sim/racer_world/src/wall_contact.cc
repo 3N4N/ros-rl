b523b0cd856f9d635a18977b814718ab6d90958f
Enan Ajmain <3nan.ajmain@gmail.com>
2022-08-04 16:32:25 +0600

    Publish collision contacts

--- src/auto_vehicle_sim/racer_world/src/wall_contact.cc
+++ src/auto_vehicle_sim/racer_world/src/wall_contact.cc
@@ -1,5 +1,7 @@
 #include <string>
 
+#include <ros/ros.h>
+#include <gazebo_msgs/ContactsState.h>
 #include <gazebo/gazebo.hh>
 #include <gazebo/sensors/sensors.hh>
 
@@ -11,7 +13,9 @@ class ContactPlugin : public SensorPlugin
 public:
   /// \brief Constructor.
   ContactPlugin() : SensorPlugin()
-  {}
+  {
+    topicName = "contacts";
+  }
 
   /// \brief Destructor.
   virtual ~ContactPlugin()
@@ -22,6 +26,16 @@ public:
   /// \param[in] _sdf SDF element that describes the plugin.
   virtual void Load(sensors::SensorPtr _sensor, sdf::ElementPtr _sdf)
   {
+    // Make sure the ROS node for Gazebo has already been initialized
+    if (!ros::isInitialized()) {
+      ROS_INFO("ROS should be initialized first!");
+      return;
+    }
+
+    if (!_sensor) {
+      gzerr << "Invalid sensor pointer.\n";
+    }
+
     // Get the parent sensor.
     this->parentSensor =
       std::dynamic_pointer_cast<sensors::ContactSensor>(_sensor);
@@ -32,6 +46,11 @@ public:
       return;
     }
 
+    ROS_INFO("The Sonar plugin has been loaded!");
+
+    node_handle_ = new ros::NodeHandle("");
+    pub_ = node_handle_->advertise<gazebo_msgs::ContactsState>(topicName, 1);
+
     // Connect to the sensor update event.
     this->updateConnection =
       this->parentSensor->ConnectUpdated(std::bind(&ContactPlugin::OnUpdate, this));
@@ -41,29 +60,31 @@ public:
   }
 
 private:
+  // FIXME: copy all the members, not just collision-link names
+  gazebo_msgs::ContactsState gzContacts2rosContacts(msgs::Contacts contacts)
+  {
+    gazebo_msgs::ContactsState rContacts;
+    gazebo_msgs::ContactState contactState;
+
+    for (unsigned int i = 0; i < contacts.contact_size(); ++i) {
+      contactState.collision1_name = contacts.contact(i).collision1();
+      contactState.collision2_name = contacts.contact(i).collision2();
+
+      rContacts.states.push_back(contactState);
+    }
+
+    return rContacts;
+  }
+
   /// \brief Callback that receives the contact sensor's update signal.
   virtual void OnUpdate()
   {
     // Get all the contacts.
     msgs::Contacts contacts;
     contacts = this->parentSensor->Contacts();
-    for (unsigned int i = 0; i < contacts.contact_size(); ++i)
-    {
-      std::cout << "Collision between[" << contacts.contact(i).collision1()
-                << "] and [" << contacts.contact(i).collision2() << "]\n";
 
-      for (unsigned int j = 0; j < contacts.contact(i).position_size(); ++j)
-      {
-        std::cout << j << "  Position:"
-                  << contacts.contact(i).position(j).x() << " "
-                  << contacts.contact(i).position(j).y() << " "
-                  << contacts.contact(i).position(j).z() << "\n";
-        std::cout << "   Normal:"
-                  << contacts.contact(i).normal(j).x() << " "
-                  << contacts.contact(i).normal(j).y() << " "
-                  << contacts.contact(i).normal(j).z() << "\n";
-        std::cout << "   Depth:" << contacts.contact(i).depth(j) << "\n";
-      }
+    if (contacts.contact_size() > 0) {
+      pub_.publish(gzContacts2rosContacts(contacts));
     }
   }
 
@@ -73,8 +94,11 @@ private:
   /// \brief Connection that maintains a link between the contact sensor's
   /// updated signal and the OnUpdate callback.
   event::ConnectionPtr updateConnection;
+
+  ros::NodeHandle *node_handle_;
+  ros::Publisher pub_;
+  std::string topicName;
 };
 
 GZ_REGISTER_SENSOR_PLUGIN(ContactPlugin)
 }
-
